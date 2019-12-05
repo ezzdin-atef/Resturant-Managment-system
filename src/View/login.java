1116801aa10d66f9495ff9af97b8b95c2ac975c6
Ezzdin Atef <ezzdin1125@gmail.com>
2019-12-05 21:47:25 +0200

    View Package

---
+++ src/View/login.java
@@ -0,0 +1,14 @@
+
+package View;
+
+import javax.swing.*;
+
+public class login {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click Me");
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
